d7f3d5a8f46c6f2776d03400ba3b365c8b381e84
Devarsh <devarshranpara0803@gmail.com>
2019-01-17 16:40:32 +0530

    [Added class for usage report]

---
+++ lib/Classes/user_usage.dart
@@ -0,0 +1,9 @@
+class UserUsage
+{
+  final String date;
+  final String startingTime;
+  final String endingTime;
+  final int use;
+
+  UserUsage(this.date,this.startingTime,this.endingTime,this.use);
+}
