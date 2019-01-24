83632a87a1925fd9d319dafc989ebfe683c0f04e
Devarsh <devarshranpara0803@gmail.com>
2019-01-24 16:13:08 +0530

    [Block button]

--- lib/CustomWidgets/user_detail.dart
+++ lib/CustomWidgets/user_detail.dart
@@ -3,191 +3,202 @@ import 'package:flutter_app/Classes/users.dart';
 import 'package:flutter_app/CustomWidgets/circular_image.dart';
 import 'package:flutter_app/CustomWidgets/expantion_tile.dart';
 
-
-
-class UserDetailTile extends StatelessWidget {
-
+class UserDetailTile extends StatefulWidget {
   final Users user;
-
   UserDetailTile(this.user);
 
   @override
+  _UserDetailTileState createState() => _UserDetailTileState();
+}
+
+class _UserDetailTileState extends State<UserDetailTile> {
+  @override
   Widget build(BuildContext context) {
+    String strblock;
+
+    if (widget.user.isBocked) {
+      strblock = "Unblock";
+    } else {
+      strblock = "Block";
+    }
     return InkWell(
-                onTap: () {},
-                child: Card(
-                    child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 5.0, vertical: 2.0),
-                        child: AppExpansionTile(
-                          title: Row(
+        onTap: () {},
+        child: Card(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 5.0, vertical: 2.0),
+                child: AppExpansionTile(
+                  title: Row(
+                    children: <Widget>[
+                      CircularImage(
+                        AssetImage("assets/dev.jpeg"),
+                        width: 30,
+                        height: 30,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.3,
+                        child: Text(widget.user.name),
+                      ),
+                      //Text(name),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        widget.user.institute,
+                        style: TextStyle(color: Colors.tealAccent),
+                      )
+                    ],
+                  ),
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.3,
+                          child: Column(
                             children: <Widget>[
-                              CircularImage(
-                                AssetImage("assets/dev.jpeg"),
-                                width: 30,
-                                height: 30,
+                              Text(
+                                "Limit",
                               ),
-                              SizedBox(width: 10.0,),
-                              SizedBox(width: MediaQuery.of(context).size.width * 0.3,
-                              child:Text(user.name), 
+                              Text(widget.user.limit.toString() + " M")
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.3,
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                "Usage",
+                                style: TextStyle(color: Colors.red),
                               ),
-                              //Text(name),
-                              SizedBox(width: 10.0,),
-                              Text(user.institute,
-                              style: TextStyle(
-                                color: Colors.tealAccent
-                              ),
+                              Text(
+                                widget.user.usage.toString() + " M",
+                                style: TextStyle(color: Colors.redAccent),
                               )
                             ],
                           ),
-                          children: <Widget>[
-                            Row(
+                        ),
+                        SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.3,
+                            child: Column(
                               children: <Widget>[
-                                SizedBox(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.3,
-                                  child: Column(
-                                    children: <Widget>[
-                                      Text(
-                                        "Limit",
-                                      ),
-                                      Text(user.limit.toString()+" M")
-                                    ],
-                                  ),
+                                Text(
+                                  "Bal",
+                                  style: TextStyle(color: Colors.teal),
                                 ),
-                                SizedBox(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.3,
-                                  child: Column(
-                                    children: <Widget>[
-                                      Text(
-                                        "Usage",
-                                        style: TextStyle(color: Colors.red),
-                                      ),
-                                      Text(
-                                        user.usage.toString()+" M",
-                                        style:
-                                            TextStyle(color: Colors.redAccent),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                SizedBox(
-                                    width:
-                                        MediaQuery.of(context).size.width * 0.3,
-                                    child: Column(
-                                      children: <Widget>[
-                                        Text(
-                                          "Bal",
-                                          style: TextStyle(color: Colors.teal),
-                                        ),
-                                        Text(
-                                          user.bal.toString()+" M",
-                                          style: TextStyle(
-                                              color: Colors.tealAccent),
-                                        )
-                                      ],
-                                    )),
+                                Text(
+                                  widget.user.bal.toString() + " M",
+                                  style: TextStyle(color: Colors.tealAccent),
+                                )
                               ],
+                            )),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.3,
+                          child: RawMaterialButton(
+                            onPressed: () {},
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 5.0, horizontal: 10.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.timeline,
+                                    size: 15.0,
+                                  ),
+                                  SizedBox(
+                                    width: 2.0,
+                                  ),
+                                  Text("Ch. Limit"),
+                                ],
+                              ),
                             ),
-                            Row(
-                              children: <Widget>[
-                                SizedBox(
-                                  width: 5.0,
-                                ),
-                                SizedBox(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.3,
-                                  child: RawMaterialButton(
-                                    onPressed: () {},
-                                    child: Padding(
-                                      padding: EdgeInsets.symmetric(
-                                          vertical: 5.0, horizontal: 10.0),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          Icon(
-                                            Icons.timeline,
-                                            size: 15.0,
-                                          ),
-                                          SizedBox(
-                                            width: 2.0,
-                                          ),
-                                          Text("Ch. Limit"),
-                                        ],
-                                      ),
-                                    ),
-                                    fillColor: Colors.blue,
-                                    splashColor: Colors.blueAccent,
-                                    shape: const StadiumBorder(),
+                            fillColor: Colors.blue,
+                            splashColor: Colors.blueAccent,
+                            shape: const StadiumBorder(),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.3,
+                          child: RawMaterialButton(
+                            onPressed: () {
+                              setState(() {
+                                if (widget.user.isBocked) {
+                                  widget.user.isBocked=false;
+                                  strblock = "Block";
+                                } else {
+                                  widget.user.isBocked=true;
+                                  strblock = "Unlock";
+                                }
+                              });
+                            },
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 5.0, horizontal: 10.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.block,
+                                    size: 15.0,
                                   ),
-                                ),
-                                SizedBox(
-                                  width: 5.0,
-                                ),
-                                SizedBox(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.3,
-                                  child: RawMaterialButton(
-                                    onPressed: () {},
-                                    child: Padding(
-                                      padding: EdgeInsets.symmetric(
-                                          vertical: 5.0, horizontal: 10.0),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          Icon(
-                                            Icons.block,
-                                            size: 15.0,
-                                          ),
-                                          SizedBox(
-                                            width: 2.0,
-                                          ),
-                                          Text("Block"),
-                                        ],
-                                      ),
-                                    ),
-                                    fillColor: Colors.orange,
-                                    splashColor: Colors.orangeAccent,
-                                    shape: const StadiumBorder(),
+                                  SizedBox(
+                                    width: 2.0,
                                   ),
-                                ),
-                                SizedBox(
-                                  width: 5.0,
-                                ),
-                                SizedBox(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.3,
-                                  child: RawMaterialButton(
-                                    onPressed: () {},
-                                    child: Padding(
-                                      padding: EdgeInsets.symmetric(
-                                          vertical: 5.0, horizontal: 10.0),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          Icon(
-                                            Icons.delete,
-                                            size: 15.0,
-                                          ),
-                                          SizedBox(
-                                            width: 2.0,
-                                          ),
-                                          Text("Delete"),
-                                        ],
-                                      ),
-                                    ),
-                                    fillColor: Colors.red,
-                                    splashColor: Colors.redAccent,
-                                    shape: const StadiumBorder(),
-                                  ),
-                                ),
-                              ],
+                                  Text(strblock),
+                                ],
+                              ),
                             ),
-                          ],
-                        ))));
+                            fillColor: Colors.orange,
+                            splashColor: Colors.orangeAccent,
+                            shape: const StadiumBorder(),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.3,
+                          child: RawMaterialButton(
+                            onPressed: () {},
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 5.0, horizontal: 10.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.delete,
+                                    size: 15.0,
+                                  ),
+                                  SizedBox(
+                                    width: 2.0,
+                                  ),
+                                  Text("Delete"),
+                                ],
+                              ),
+                            ),
+                            fillColor: Colors.red,
+                            splashColor: Colors.redAccent,
+                            shape: const StadiumBorder(),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ))));
   }
 }
--- lib/Models/manage_users_model.dart
+++ lib/Models/manage_users_model.dart
@@ -7,13 +7,13 @@ class ManageUsersModel
   ManageUsersModel()
   {
     user=[
-      Users(name:"Dhaval",institute: "LJ MCA",url:"", limit: 200,usage: 50,bal: 150),
-      Users(name:"Kanti",institute: "LJ MBA",url:"", limit: 200,usage: 50,bal: 150),
-      Users(name:"Devang",institute: "LJ Engg.",url:"", limit: 200,usage: 50,bal: 150),
-      Users(name:"Nilesh",institute: "LJ Art.",url:"", limit: 200,usage: 50,bal: 150),
-      Users(name:"Jayesh",institute: "LJ Com.",url:"", limit: 200,usage: 50,bal: 150),
-      Users(name:"Chirag",institute: "LJ B.sc.",url:"", limit: 200,usage: 50,bal: 150),
-      Users(name:"Ravi",institute: "LJ MBA",url:"", limit: 200,usage: 50,bal: 150),
+      Users(name:"Dhaval",institute: "LJ MCA",url:"", limit: 200,usage: 50,bal: 150,isBocked: false),
+      Users(name:"Kanti",institute: "LJ MBA",url:"", limit: 200,usage: 50,bal: 150,isBocked: true),
+      Users(name:"Devang",institute: "LJ Engg.",url:"", limit: 200,usage: 50,bal: 150,isBocked: false),
+      Users(name:"Nilesh",institute: "LJ Art.",url:"", limit: 200,usage: 50,bal: 150,isBocked: false),
+      Users(name:"Jayesh",institute: "LJ Com.",url:"", limit: 200,usage: 50,bal: 150,isBocked: false),
+      Users(name:"Chirag",institute: "LJ B.sc.",url:"", limit: 200,usage: 50,bal: 150,isBocked: true),
+      Users(name:"Ravi",institute: "LJ MBA",url:"", limit: 200,usage: 50,bal: 150,isBocked: false),
     ];
   }
 
