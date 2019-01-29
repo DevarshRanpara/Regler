f20ed068adfc0ac05d9730aa33cad6b1de604ab1
Devarsh <devarshranpara0803@gmail.com>
2019-01-29 14:23:34 +0530

    []

--- lib/Classes/admin_data.dart
+++
@@ -1,6 +0,0 @@
-class AdminData {
-  String name;
-  String role;
-
-  AdminData({this.name, this.role});
-}
--- lib/Models/view_usage_user_model.dart
+++ lib/Models/view_usage_user_model.dart
@@ -1,5 +1,8 @@
-
+import 'package:flutter_app/Classes/user_data.dart';
+import 'package:flutter_app/Classes/user_usage.dart';
 class ViewUsageUser
 {
-
+  UserData data;
+  UserUsage usage;
+  
 }
