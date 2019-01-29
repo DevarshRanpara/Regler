780b96b0b050d263b91e6b4e4ec5466dbe0fd9d3
Devarsh <devarshranpara0803@gmail.com>
2019-01-29 11:38:04 +0530

    [Admin][Added view complain page]

---
+++ lib/Classes/complain.dart
@@ -0,0 +1,10 @@
+class Complain
+{
+  int id;
+  String acNo;
+  String uname;
+  String complain;
+
+  Complain({this.id,this.uname,this.complain,this.acNo});
+
+}
--- lib/Views/Admin/view_complain_view.dart
+++ lib/Views/Admin/view_complain_view.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/Classes/complain.dart';
+import 'package:flutter_app/CustomWidgets/Admin/complain_tile.dart';
+import 'package:flutter_app/Models/view_complain_model.dart';
 
-class ViewComplainView extends StatelessWidget {
+
+class ViewComplainView extends StatefulWidget {
+  @override
+  _ViewComplainViewState createState() => _ViewComplainViewState();
+}
+
+class _ViewComplainViewState extends State<ViewComplainView> {
+
+  ViewComplainModel model=ViewComplainModel();
+  List<Complain> complains=List();
+  List<ComplainTile> complainTile=List();
+
+  @override
+    void initState() {
+      complains=model.getData();
+      for(int i=0;i<complains.length;i++)
+      {
+        complainTile.add(ComplainTile(complains[i]));
+      }
+      super.initState();
+    }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -9,9 +33,47 @@ class ViewComplainView extends StatelessWidget {
             fontFamily: 'Montserrat',
             accentColor: Colors.teal),
         home: Scaffold(
-          body: Center(
-            child: Text("ViewComplainView"),
-          ),
+          body: Container(
+            child: ListView(
+              children: <Widget>[
+                Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
+              ),
+              InkWell(
+                  onTap: () {},
+                  child: Card(
+                      child: Container(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: <Widget>[
+                        Icon(
+                          Icons.supervisor_account,
+                          color: Colors.redAccent,
+                          size: 35.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          "View Complains",
+                          style: TextStyle(
+                              color: Colors.tealAccent,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w200),
+                        )
+                      ],
+                    ),
+                  ))),
+                  Column(children: complainTile)
+              ],
+            ),
+          )
         ));
   }
 }
+
+
+
+
+ 
