b9da08d73dac727be3c1c5826cc082fb83f866df
Devarsh <devarshranpara0803@gmail.com>
2019-02-04 13:53:42 +0530

    []

--- lib/Models/login_model.dart
+++ lib/Models/login_model.dart
@@ -5,43 +5,43 @@ class LoginModel {
   String _userid, _password;
   LoginModel(this._userid, this._password);
 
-  // Future<String> auth() async {
+  Future<String> auth() async {
 
-  //   try{
-  //     FirebaseUser user=await FirebaseAuth.instance.signInWithEmailAndPassword(email: _userid, password: _password);
-  //     //Firestore.instance.collection("/users").where('uid')
-  //     print(user.uid+"\n\n\n"+user.email);
-  //     if(_userid=="dev@mail.com"){
-  //       return "user";
-  //     }
-  //     else if(_userid=="admin@mail.com"){
-  //       return "admin";
-  //     }
-  //     else{
-  //       return "director";
-  //     }
-  //   }
-  //   catch(e){
-  //     print(e.toString());
-  //     return "Invalid";
-  //   }
-
-    
-
-
-    
-  // }
-
-  String auth()
-  {
-    if (_userid == 'user' && _password == "password1") {
-      return 'user';
-    } else if (_userid == 'admin' && _password == "password1") {
-      return 'admin';
-    } else if (_userid == 'director' && _password == "password1") {
-      return 'director';
-    } else {
-      return "invalid";
+    try{
+      FirebaseUser user=await FirebaseAuth.instance.signInWithEmailAndPassword(email: _userid, password: _password);
+      //Firestore.instance.collection("/users").where('uid')
+      print(user.uid+"\n\n\n"+user.email);
+      if(_userid=="dev@mail.com"){
+        return "user";
+      }
+      else if(_userid=="admin@mail.com"){
+        return "admin";
+      }
+      else{
+        return "director";
+      }
     }
+    catch(e){
+      print(e.toString());
+      return "Invalid";
+    }
+
+    
+
+
+    
   }
+
+  // String auth()
+  // {
+  //   if (_userid == 'user' && _password == "password1") {
+  //     return 'user';
+  //   } else if (_userid == 'admin' && _password == "password1") {
+  //     return 'admin';
+  //   } else if (_userid == 'director' && _password == "password1") {
+  //     return 'director';
+  //   } else {
+  //     return "invalid";
+  //   }
+  // }
 }
