6b0dc354d33591e5b4ff4ed5126d7b8743ff420c
Devarsh <devarshranpara0803@gmail.com>
2019-02-15 10:40:32 +0530

    [URL changes]

---
+++ lib/Classes/gen_string.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_app/Classes/preferances.dart';
+import 'package:flutter_app/Classes/url_link.dart';
+
+class GenerateString {
+  static String genStringLogin(String method, String uname, String pass) {
+    return UrlLink.url+'?method=' +
+        method +
+        '&uname=' +
+        uname +
+        '&pass=' +
+        pass;
+  }
+
+  static genStringChpass(String oldpass, String newpass) {
+    return UrlLink.url+'?method=chpass&uid=' +
+        Preferances.id.toString() +
+        '&oldpass=' +
+        oldpass +
+        '&newpass=' +
+        newpass;
+  }
+
+  static String genStringBlock(String id) {
+    return UrlLink.url+'?method=block&id=' +
+        id;
+  }
+
+  static String genStringMngUsers() {
+    return UrlLink.url+'?method=mngusers&id=' +
+        Preferances.id.toString();
+  }
+}
