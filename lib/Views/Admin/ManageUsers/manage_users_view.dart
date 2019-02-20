a0d831ca13174be8f7014ab5de058e9b99b7cf8c
Devarsh <devarshranpara0803@gmail.com>
2019-02-20 18:15:45 +0530

    [Added bottom nevigation manage users]

--- lib/Views/Admin/ManageUsers/manage_users_view.dart
+++ lib/Views/Admin/ManageUsers/manage_users_view.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/Classes/preferances.dart';
-import 'package:flutter_app/Classes/user_data.dart';
 import 'package:flutter_app/CustomWidgets/Admin/dialouges_user.dart';
-import 'package:flutter_app/Models/manage_users_model.dart';
-import 'package:flutter_app/CustomWidgets/Admin/user_detail.dart';
-import 'add_user_view.dart';
+import 'package:flutter_app/Views/Admin/ManageUsers/add_user_view.dart';
+import 'package:flutter_app/Views/Admin/ManageUsers/manage_user.dart';
 
 //import 'package:flutter_app/Resources/bloc.dart';
 
@@ -14,38 +11,27 @@ class ManageUsersView extends StatefulWidget {
 }
 
 class _ManageUsersViewState extends State<ManageUsersView> {
-  List<UserDetailTile> list = List();
-  ManageUsersModel model;
-  List<UserData> user;
-  Stream<List<UserData>> stream;
+  List<Widget> pages;
+  int _selectedIndex = 0;
+
   Dialogs dialogs;
 
   @override
   void initState() {
+
     dialogs = Dialogs(context);
-    model = ManageUsersModel();
-    stream = Stream.fromFuture(model.getData());
-    //bloc.fetchAllUsers();
+    
+    Widget user = ManageUser(addUser,dialogs);
+    Widget dir = Text('Director');
+    pages = [user, dir];
     super.initState();
   }
 
-  @override
-  void dispose() {
-    //bloc.dispose();
-    Preferances.building.clear();
-    super.dispose();
-  }
-
-  void blockUser(UserData user) {
-    dialogs.showAlertUserBlock(user);
-  }
-
-  void deleteUser(UserData user) {
-    dialogs.showAlertUserDelete(user);
-  }
-
-  void changeLimit(UserData user) {
-    dialogs.showAlertUserChlimit(user);
+  addUser(){
+      Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => AddUserView()));
   }
 
   @override
@@ -56,111 +42,26 @@ class _ManageUsersViewState extends State<ManageUsersView> {
             fontFamily: 'Montserrat',
             accentColor: Colors.teal),
         home: Scaffold(
-            body: Container(
-          child: ListView(
-            children: <Widget>[
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.95,
-                child: StreamBuilder(
-                  //stream: bloc.allUsers,
-                  stream: stream,
-                  builder: (BuildContext context, AsyncSnapshot snapshot) {
-                    if (snapshot.data == null) {
-                      return Center(
-                        child: SizedBox(
-                          height: 30,
-                          width: 30,
-                          child: CircularProgressIndicator(),
-                        ),
-                      );
-                    } else {
-                      return ListView.builder(
-                        itemCount: snapshot.data.length + 1,
-                        itemBuilder: (BuildContext conext, int i) {
-                          if (i == 0) {
-                            return getUpperUI();
-                          }
-                          return UserDetailTile(snapshot.data[i - 1], blockUser,
-                              deleteUser, changeLimit);
-                        },
-                      );
-                    }
-                  },
-                ),
-              )
-            ],
+          body: Container(
+            child: pages.elementAt(_selectedIndex),
           ),
-        )));
+          bottomNavigationBar: BottomNavigationBar(
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.supervisor_account), title: Text("User")),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.verified_user), title: Text('Director'))
+            ],
+            currentIndex: _selectedIndex,
+            fixedColor: Colors.tealAccent,
+            onTap: _onItemTapped,
+          ),
+        ));
   }
 
-  Widget getUpperUI() {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      child: Column(
-        children: <Widget>[
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
-          ),
-          InkWell(
-              onTap: () {},
-              child: Card(
-                  child: Container(
-                padding: const EdgeInsets.all(20.0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      Icon(
-                        Icons.supervisor_account,
-                        color: Colors.redAccent,
-                        size: 35.0,
-                      ),
-                      SizedBox(
-                        height: 15.0,
-                      ),
-                      Text(
-                        "Manage Users",
-                        style: TextStyle(
-                            color: Colors.tealAccent,
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.w200),
-                      )
-                    ],
-                  ),
-                ),
-              ))),
-          InkWell(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => AddUserView()));
-              },
-              child: Card(
-                child: Container(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.add,
-                        color: Colors.amber,
-                      ),
-                      SizedBox(
-                        width: 8.0,
-                      ),
-                      Text(
-                        "Add User",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-              )),
-        ],
-      ),
-    );
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
