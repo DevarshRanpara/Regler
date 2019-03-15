21b8040e89b16686660e01b967ec51b9d041fad3
Devarsh <devarshranpara0803@gmail.com>
2019-03-15 12:51:54 +0530

    [Little changes]

--- lib/CustomWidgets/Admin/room_usage_tile.dart
+++ lib/CustomWidgets/Admin/room_usage_tile.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/Classes/room.dart';
 
 class RoomUsageTile extends StatefulWidget {
+
+  final Function gotoRoomUsage;
+
   final Room room;
 
-  RoomUsageTile(this.room);
+  RoomUsageTile(this.room,this.gotoRoomUsage);
 
   @override
   _RoomTileState createState() => _RoomTileState();
@@ -14,23 +17,33 @@ class _RoomTileState extends State<RoomUsageTile> {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {widget.gotoRoomUsage();},
       child: Card(
         child: Container(
           padding: EdgeInsets.all(20.0),
           child: Row(
             children: <Widget>[
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.crop_square,
+                      color: Colors.redAccent,
+                      size: 25.0,
+                    ),
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Text(
+                      widget.room.name,
+                      style: TextStyle(fontSize: 15.0),
+                    ),
+                  ],
+                ),
+              ),
               Icon(
-                Icons.crop_square,
-                color: Colors.redAccent,
-                size: 25.0,
-              ),
-              SizedBox(
-                width: 15.0,
-              ),
-              Text(
-                widget.room.name,
-                style: TextStyle(fontSize: 15.0),
+                Icons.arrow_forward_ios,
+                color: Colors.red[500],
               ),
             ],
           ),
